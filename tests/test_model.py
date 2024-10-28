80893f6b04f17394b065e81150235e953f8967e6
dianna-SE <diannapham.se@gmail.com>
2024-10-28 14:30:21 -0700

    added audio WAV files to test in CLI through unit testing, and added several unit tests to test validity of WAV file.

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,15 +4,18 @@
 #  Adapted from source URL:
 #       https://theaisummer.com/unit-test-deep-learning/
 
+
 # Ideas to test:
-#   - Test if file is corrupted or not before passing to neural network model
+#   - Test if file is corrupted or not before passing
+#       to neural network model (Some checks made)
 #   - Test if model loads successfully with servers
-#   - Test that web application rejects non-WAV files
 #   - Edge cases (empty files, WAV but no sound, no file uploaded, etc.)
+#          (Some checks made)
 #   - Test that audio file is less than X seconds (not too long) or if able
 #       to handle large files
 
 import unittest
+import wave
 import torch
 
 # may need to convert the model in .ipynb file to .py to properly test model ?
@@ -32,6 +35,7 @@ class ModelTest(unittest.TestCase):
         # (file to path -- assuming model is in .pth PYTORCH file)
         model_path = "../model.pth"
 
+        # try block for less cryptic error
         try:
             model = torch.load(model_path, weights_only=True)
             model.eval()
@@ -47,11 +51,84 @@ class ModelTest(unittest.TestCase):
             print(f"ERROR: Failed to load model: {str(event)}")
             return None
 
+    # Testing audio input and model in CLI (through unit testing)
     def test_invalid_model(self):
         """Tests if the model exists when loading it."""
         model = self.load_model()
         self.assertIsNotNone(model,
                              "ERROR: Invalid or model file does not exist.")
+
+    # Author: Python
+    # Date Accessed: October 28, 2024
+    # Documentation that provides functions and exceptions to reading and
+    #   writing WAV files.
+    # Adapted from source URL:
+    #      https://docs.python.org/3/library/wave.html
+    def open_wav(self):
+        """Method that opens a WAV file and returns it for testing."""
+
+        # working WAV
+        wav_path = (
+            "./audio_datasets/Joint_C_Beat_Laboratory_"
+            "War_with_Yourself.wav"
+            )
+
+        # invalid WAV (corrupt)
+        # wav_path = ("./audio_datasets/corrupt_file.wav")
+        return wave.open(wav_path, mode='rb')
+
+    # Tests several components of a WAV file for corruption
+    def test_wav_no_channel(self):
+        """Tests if the WAV file has no channel. This can indicate that the
+        WAV file has no audible sound."""
+
+        # call func to open file path
+        wav_file = self.open_wav()
+
+        # returns  number of audio channels:
+        # 1 (mono), 2 (stereo), or 3 (surround sound)
+        channels = wav_file.getnchannels()
+
+        # no channel indicates a possible issue with WAV file (no sound)
+        print("Channels: ", channels)
+        self.assertGreater(channels, 0, f'ERROR: No channel detected. '
+                           f'Channels: {channels}')
+
+        wav_file.close()
+
+    def test_wav_no_frame_rate(self):
+        """Tests if the WAV file has a frame rate (sampling frequency)."""
+        wav_file = self.open_wav()
+
+        # gets sampling frequency (Hz)
+        frame_rates = wav_file.getframerate()
+
+        # having no frame rate indicates no frequency to the WAV -
+        # sound could be distorted, missing or corrupt
+        print("Frame Rates: ", frame_rates)
+        self.assertGreater(frame_rates, 0, f'ERROR: No audio frames found. '
+                           f'Sampling frequency: {frame_rates}')
+        wav_file.close()
+
+    def test_wav_no_audio_frames(self):
+        """Tests if the WAV file is empty (no audio)."""
+        wav_file = self.open_wav()
+
+        # gets the number of audio frames -
+        audio_frames = wav_file.getnframes()
+
+        # having no audio frames indicate frame count is missing
+        #   (invalid or corrupt)
+        print("Audio Frames: ", audio_frames)
+        self.assertGreater(audio_frames, 0,
+                           f'ERROR: No audio frames found. '
+                           f'Number of frames: {audio_frames}')
+
+        wav_file.close()
+
+    def test_model_prediction(self):
+        """Tests if the model predictions are accurate."""
+        pass
 
 
 if __name__ == '__main__':
