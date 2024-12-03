bbd19bc8c016580b5ed0dd43b50c3c4bf101c98a
dianna-SE <diannapham.se@gmail.com>
2024-12-03 01:50:59 -0800

    assigned dynamic port

--- backend/app.py
+++ backend/app.py
@@ -150,4 +150,5 @@ def root():
 
 # Run the app in development mode
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    PORT = int(os.environ.get('PORT', 8000))
+    app.run(debug=True, host="0.0.0.0", port=PORT)
