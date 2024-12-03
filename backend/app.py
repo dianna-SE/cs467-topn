2c564bc7b09ba26ce02b7104e5db4ca6bf6c0ab7
dianna-SE <diannapham.se@gmail.com>
2024-12-03 02:23:52 -0800

    added accept to headers

--- backend/app.py
+++ backend/app.py
@@ -140,12 +140,12 @@ def root():
 
 
 # Handle CORS for responses
-# @app.after_request
-# def after_request(response):
-#     response.headers['Access-Control-Allow-Origin'] = '*'
-#     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-#     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-#     return response
+@app.after_request
+def after_request(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, Accept'
+    return response
 
 
 # Run the app in development mode
